2a1d820d0fde93dda8ccabcaf7e92f48df2e9f74
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-04-23 08:31:27 -0400

    Updated from Brython Server: 4/23/2018 8:31:27 AM

--- middleWord.py
+++ middleWord.py
@@ -1,1 +1,12 @@
+#Heather Stafford
+#4/23/18
+#middleword.py
 
+words = input('Enter some words: ').split(' ')
+
+num = int(len(words)%2)
+
+if num = 0:
+    print(words[num])
+elif num != 0:
+    print(words)
