667b98f002998b2c13a54956ea64fcc024d1699b
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-04-25 08:06:14 -0400

    Updated from Brython Server: 4/25/2018 8:06:13 AM

--- dictionaryDemo.py
+++ dictionaryDemo.py
@@ -1,1 +1,11 @@
+#Heather Stafford
+#4/25/18
+#dictionaryDemo.py - most list practice
+
+words = ['computer','mortify','dog','firetruck','yes','python','cat']
+
+words.sort()
+
+num = int(input('What number word do you want? '))
+
 
