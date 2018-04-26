e9540a898433a24526e4a6106e0ccca3e322ae93
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-04-26 08:42:55 -0400

    Updated from Brython Server: 4/26/2018 8:42:54 AM

--- randomMonth.py
+++ randomMonth.py
@@ -6,6 +6,6 @@ from random import randint
 
 num = randint(1,12)
 
-months = ['January', 'Feburary', 'March','April','May','June','July','August','September','October','November','December']
+months = ['January','Feburary','March','April','May','June','July','August','September','October','November','December']
 
 print(months[num: num+1])
