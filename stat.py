a4bf5e6989e5e4c35b6f7172fffa041c23fb82a6
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-04-26 08:39:05 -0400

    Updated from Brython Server: 4/26/2018 8:39:03 AM

--- stat.py
+++ stat.py
@@ -2,8 +2,7 @@
 #4/26/18
 #stat.py
 
-numbers = int(input('Enter some numbers')).split(' ')
+numbers = input('Enter some numbers').split(' ')
 
 print(max(numbers))
 print(min(numbers))
-print(sum(numbers))
