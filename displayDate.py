bc5455a6cd1fcdac32045a13c26bbb258cecbe19
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-04-30 08:41:04 -0400

    Updated from Brython Server: 4/30/2018 8:41:04 AM

--- displayDate.py
+++ displayDate.py
@@ -1,1 +1,12 @@
+#Heather Stafford
+#4/30/18
+#displatDate.py
 
+from datetime import *
+
+today = datetime.date.today()
+weekday = today.weekday()
+month = datetime.date.month()
+year = datetime.date.year()
+
+print('The date is ',weekday, todat, month, year)
