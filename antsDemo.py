94849a97b5329ede3a05ad59931649cbe432e9b8
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-04-30 08:06:54 -0400

    Updated from Brython Server: 4/30/2018 8:06:54 AM

--- antsDemo.py
+++ antsDemo.py
@@ -1,1 +1,16 @@
+#Heather Stafford
+#4/30/18
+#antsDemo.py
 
+from ggame import *
+
+ANTS = 10
+
+if __name__ == '__main__':
+    
+    red = Color(0xff0000,1)
+    ant = CircleAsset(10,LineStyle(1,red),red)
+    
+    Sprite(ant)
+    
+    App().run()
