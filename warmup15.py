67152287b52f76aff9b8cf07481b9ee1f948b3f7
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-05-04 08:16:09 -0400

    Updated from Brython Server: 5/4/2018 8:16:08 AM

--- warmup15.py
+++ warmup15.py
@@ -1,1 +1,9 @@
+#Heather Stafford
+#5/4/18
+#warmup15.py
 
+def numbersdouble():
+    numberlist = numbersdouble.split(' ')
+    for i in range(0,numberlist.count()):
+        numberlist[i] = (numberlist[i] * 2)
+    print(numberlist)
