7676e3435246ee36477e376e12b85a8199b2afa2
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-05-07 08:21:15 -0400

    Updated from Brython Server: 5/7/2018 8:21:14 AM

--- unit5Quiz.py
+++ unit5Quiz.py
@@ -1,1 +1,25 @@
+#Heather Stafford
+#5/7/18
+#quiz5.py
 
+def penultimate(L):
+    L[int(len(L)) - 1]
+    
+print(penultimate([3,4,5,6,7]))
+
+
+def plusEquals(A, B):
+    new = []
+    for item in A:
+        new.append(item + B) 
+    return new
+    
+smallest = 0
+def smallest(C):
+    for item in C:
+        if item > smallest:
+            smallest = smallest + item
+    
+
+print(plusEquals([1,2,3,4],10))
+print(smallest([1,2,3,4]))
