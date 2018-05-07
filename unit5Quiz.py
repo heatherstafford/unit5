2e5b06865d415fa45028f2ca670b37c3f227ae19
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-05-07 08:28:19 -0400

    Updated from Brython Server: 5/7/2018 8:28:19 AM

--- unit5Quiz.py
+++ unit5Quiz.py
@@ -3,7 +3,7 @@
 #quiz5.py
 
 def penultimate(L):
-    L[int(len(L)) - 1]
+    L[len(L) - 1]
     
 print(penultimate([3,4,5,6,7]))
 
@@ -13,15 +13,19 @@ def plusEquals(A, B):
     for item in A:
         new.append(item + B) 
     return new
-    
 
 def smallest(C):
-    small = 0
+    small = 100
     for item in C:
-        if item > small:
-            small = small + item
-        
+        if item < small:
+            small = item
+    return small
     
+def decimalRange(D):
+    F = []
+    for i in range(float(D[0]), float(D[1]), float(D[2])):
+        print(i)
 
 print(plusEquals([1,2,3,4],10))
 print(smallest([1,2,3,4]))
+print(decimalRange([4,10,0.5]))
